ac8cb9da55d1cb6956668d30c2b38869a485a4ed
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2023-11-01 10:20:59 +0200

    page products

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,141 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <v-app id="app">
     <v-main>
-      <HelloWorld/>
+      <v-container fluid>
+        <v-row justify="center" class="mb-0 row_tabs">
+          <v-col
+            cols="6"
+            lg="9"
+            class="pa-0"
+            :class="page == 1 && !$vuetify.breakpoint.lgAndUp ? 'white' : ''"
+          >
+            <div
+              v-show="!$vuetify.breakpoint.lgAndUp"
+              class="tab py-3"
+              :class="page == 0 ? 'active-tab-1' : 'other-tab-1'"
+              @click="page = 0"
+            >
+              Products
+            </div>
+            <div v-show="$vuetify.breakpoint.lgAndUp" class="tab_lg py-3 white">
+              Products
+            </div>
+          </v-col>
+          <v-col
+            cols="6"
+            lg="3"
+            class="pa-0"
+            :class="page == 0 && !$vuetify.breakpoint.lgAndUp ? 'white' : ''"
+          >
+            <div
+              v-show="!$vuetify.breakpoint.lgAndUp"
+              class="tab py-3"
+              :class="page == 1 ? 'active-tab-2' : 'other-tab-2'"
+              @click="page = 1"
+            >
+              Feedback
+            </div>
+            <div v-show="$vuetify.breakpoint.lgAndUp" class="tab_lg py-3">
+              Feedback
+            </div>
+          </v-col>
+        </v-row>
+        <hr class="separation" />
+        <v-row justify="center">
+          <v-col cols="12" lg="9">
+            <Products />
+          </v-col>
+          <v-col cols="12" lg="3">
+            <Feedback />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Products from "./components/Products";
+import Feedback from "./components/Feedback";
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    Products,
+    Feedback,
   },
-
   data: () => ({
-    //
+    page: 0,
   }),
 };
 </script>
+
+<style scoped>
+#app {
+  background: #fff;
+}
+.row_tabs {
+  background: #f8f8f8;
+}
+.tab {
+  width: 100%;
+}
+.active-tab-1 {
+  background: #fff;
+  color: #000;
+  text-align: center;
+  font-family: "Roboto";
+  font-size: 20px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  border-top-right-radius: 16px;
+}
+.other-tab-1 {
+  background: #f8f8f8;
+  color: #000;
+  text-align: center;
+  font-family: "Roboto";
+  font-size: 20px;
+  font-weight: 400;
+  letter-spacing: 0.4px;
+  border-bottom-right-radius: 16px;
+}
+.active-tab-2 {
+  background: #fff;
+  color: #000;
+  text-align: center;
+  font-family: "Roboto";
+  font-size: 20px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  border-top-left-radius: 16px;
+}
+.other-tab-2 {
+  background: #f8f8f8;
+  color: #000;
+  text-align: center;
+  font-family: "Roboto";
+  font-size: 20px;
+  font-weight: 400;
+  letter-spacing: 0.4px;
+  border-bottom-left-radius: 16px;
+}
+.other-tab-1:hover,
+.other-tab-1:focus-visible,
+.other-tab-2:hover,
+.other-tab-2:focus-visible {
+  color: #0063be;
+  text-decoration: underline dotted;
+}
+.tab_lg {
+  color: #000;
+  text-align: center;
+  font-family: "Roboto";
+  font-size: 24px;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+.separation {
+  border: solid 1px rgba(248, 248, 248, 1);
+}
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-row class="text-center mt-8">
+    <v-col cols="12" sm="6" lg="4" v-for="card in cards" :key="card.id">
+      <v-card flat class="pa-6 cards" :class="card.class">
+        <v-img :src="card.image" width="85%" class="mx-auto"> </v-img>
+        <v-card-title
+          class="justify-center my-4 cards-title"
+          :style="'color: ' + card.textColor"
+        >
+          {{ card.title }}
+        </v-card-title>
+        <v-card-subtitle
+          :style="'color: ' + card.textColor"
+          class="cards-subtitle"
+        >
+          {{ card.subtitle }}</v-card-subtitle
+        >
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import helicopterOrange from "@/assets/svg/img-helicopter-orange.svg";
+import helicopterGreen from "@/assets/svg/img-helicopter-green.svg";
+import airplane from "@/assets/svg/img-airplane.svg";
+import rocketGreen from "@/assets/svg/img-rocket-green.svg";
+import rocketBlue from "@/assets/svg/img-rocket-blue.svg";
+import ufo from "@/assets/svg/img-ufo.svg";
+
+export default {
+  name: "Products-Component",
+  data: () => ({
+    cards: [
+      {
+        id: "0",
+        title: "myProduct",
+        subtitle: "Sunny Orange",
+        image: helicopterOrange,
+        textColor: "#E76101",
+        class: "orangeCard",
+      },
+      {
+        id: "1",
+        title: "myProduct Plus",
+        subtitle: "Deep Blue",
+        image: airplane,
+        textColor: "#0465AC",
+        class: "blueCard",
+      },
+      {
+        id: "2",
+        title: "myProduct Pro",
+        subtitle: "Floral Green",
+        image: rocketGreen,
+        textColor: "#018D31",
+        class: "greenCard",
+      },
+      {
+        id: "3",
+        title: "myProduct Ultra",
+        subtitle: "Interstellar Grey",
+        image: ufo,
+        textColor: "#FFF",
+        class: "blackCard",
+      },
+      {
+        id: "4",
+        title: "myProduct",
+        subtitle: "Floral Green",
+        image: helicopterGreen,
+        textColor: "#018D31",
+        class: "greenCard",
+      },
+      {
+        id: "5",
+        title: "myProduct Pro",
+        subtitle: "Deep Blue",
+        image: rocketBlue,
+        textColor: "#0465AC",
+        class: "blueCard",
+      },
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+.cards {
+  border-radius: 16px;
+}
+.cards-title {
+  font-family: "Roboto";
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 32px;
+}
+.cards-subtitle {
+  font-family: "Roboto";
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 24px;
+}
+.orangeCard {
+  background-color: #fff6ce;
+}
+.orangeCard:hover {
+  background-color: #fff0b9;
+}
+.orangeCard:focus-visible {
+  border: 2px solid #e76101;
+}
+.blueCard {
+  background-color: #a2d7e7;
+}
+.blueCard:hover {
+  background-color: #92d8ff;
+}
+.blueCard:focus-visible {
+  border: 2px solid #0465ac;
+}
+.greenCard {
+  background-color: #adedb3;
+}
+.greenCard:hover {
+  background-color: #8ffb99;
+}
+.greenCard:focus-visible {
+  border: 2px solid #018d31;
+}
+.blackCard {
+  background-color: #46454a;
+}
+.blackCard:hover {
+  background-color: #3f3664;
+}
+.blackCard:focus-visible {
+  border: 2px solid #37006d;
+}
+</style>
